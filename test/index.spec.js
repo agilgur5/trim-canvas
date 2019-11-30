664b2f5584dc98ded4587a71041a19319d57b98f
Anton Gilgur <agilgur5@gmail.com>
2019-11-30 12:06:05 -0500

    (test): ensure edge case of all whitespace is handled
    - currently returns a 1x1 canvas... should probably be 0x0, but that
      requires some refactoring and may be a breaking change if anyone
      is relying on that behavior
      - reliance on that behavior may certainly be possible given how long
        this library has gone without a release, though no one's reported
        that as a bug either
      - it's a pretty big edge case, so I'm not sure that it matters much
        either way tbh
    
    - also, this gets us to 100% code coverage!!

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,7 @@
 import trimCanvas from '../index.js'
 
 describe('trimCanvas', () => {
-  it('should trim whitespace', () => {
+  it('should trim whitespace (and nothing else)', () => {
     const canvas = document.createElement('canvas')
     const ctx = canvas.getContext('2d')
     canvas.width = 1000
@@ -19,5 +19,16 @@ describe('trimCanvas', () => {
     trimCanvas(canvas)
     expect(canvas.width).toBe(200)
     expect(canvas.height).toBe(200)
+  })
+
+  it('should trim everything if only whitespace', () => {
+    const canvas = document.createElement('canvas')
+    canvas.width = 1000
+    canvas.height = 1000
+
+    trimCanvas(canvas)
+    // should probably be 0, not 1, but that's easier said than done: https://github.com/agilgur5/trim-canvas/pull/9#discussion_r352376718
+    expect(canvas.width).toBe(1)
+    expect(canvas.height).toBe(1)
   })
 })
